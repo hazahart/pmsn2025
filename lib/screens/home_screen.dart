d4d6f57390029fef2c21a031fb63f6c5db71c5ed
Gustavo Ramirez Mireles <gusramireles@hotmail.com>
2025-09-04 22:26:27 -0600

    Cambio del menu inferior en HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,43 +7,69 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _buildIcon(IconData icon, int index) {
+    bool isSelected = _selectedIndex == index;
+    return GestureDetector(
+      onTap: () => _onItemTapped(index),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        decoration: BoxDecoration(
+          color: isSelected
+              ? Colors.blue.withValues(alpha: 0.2)
+              : Colors.transparent,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: AnimatedScale(
+          scale: isSelected ? 1.3 : 1.0,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          child: Icon(
+            icon,
+            color: isSelected ? Colors.blue : Colors.grey[600],
+            size: 28,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Ci')),
-      body: Center(child: Text('Menu de opciones')),
+      appBar: AppBar(title: const Text('Ci')),
+      body: Center(child: Text('Seleccionaste $_selectedIndex')),
       drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.all(Radius.circular(50)),
+        ),
+        child: const Icon(Icons.dark_mode),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
+        shadowColor: Colors.amber,
+        elevation: 5,
         shape: const CircularNotchedRectangle(),
         notchMargin: 6.0,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            IconButton(
-              tooltip: "Home",
-              icon: const Icon(Icons.home),
-              onPressed: () {},
-            ),
-            IconButton(
-              tooltip: "Search",
-              icon: const Icon(Icons.search),
-              onPressed: () {},
-            ),
-            FloatingActionButton(
-              onPressed: () {},
-              child: Icon(Icons.dark_mode),
-            ),
-            IconButton(
-              tooltip: "Notifications",
-              icon: const Icon(Icons.notifications),
-              onPressed: () {},
-            ),
-            IconButton(
-              tooltip: "Profile",
-              icon: const Icon(Icons.person),
-              onPressed: () {},
-            ),
+            _buildIcon(Icons.home, 0),
+            _buildIcon(Icons.search, 1),
+            const SizedBox(width: 48),
+            _buildIcon(Icons.notifications, 2),
+            _buildIcon(Icons.person, 3),
           ],
         ),
       ),
